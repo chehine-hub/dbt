9129780b387b108e6b4877a09fd34d7d27846f40
Chehine MAROUANI <chehine_marouani@hotmail.com>
2021-05-01 12:55:38 -0400

    cc

--- dbt/macros/apply.sql
+++
@@ -1,8 +0,0 @@
-{% macro apply() %}
-{% set sql %}
-    CREATE OR REPLACE DATABASE DB_DEMO_DBT clone CLONE_DB_DEMO_DBT;
-{% endset %}
-
-{% do run_query(sql) %}
-{% do log("Changes applied successfully", info=True) %}
-{% endmacro %}
