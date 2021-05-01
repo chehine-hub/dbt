1694a28c3de2936a06d962dd2bc9ed157f5d3cdc
Chehine MAROUANI <chehine_marouani@hotmail.com>
2021-05-01 12:42:26 -0400

    cc

---
+++ dbt/macros/drop_clone.sql
@@ -0,0 +1,8 @@
+{% macro drop_clone() %}
+{% set sql %}
+    DROP DATABASE CLONE_DB_DEMO_DBT;
+{% endset %}
+
+{% do run_query(sql) %}
+{% do log("Clone Dropped successfully", info=True) %}
+{% endmacro %}
