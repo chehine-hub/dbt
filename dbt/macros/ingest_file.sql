43dab5d59040e5ddd6abf5df609efc48a6bbdc42
Chehine MAROUANI <chehine_marouani@hotmail.com>
2021-05-01 12:32:41 -0400

    cc

--- dbt/macros/ingest_file.sql
+++ dbt/macros/ingest_file.sql
@@ -2,7 +2,7 @@
 {% set sql %}
 COPY INTO "DB_DEMO_DBT"."RAW"."LISTE_AGENTS" 
 FROM @"DB_DEMO_DBT"."RAW"."STAGE_DEMO_DBT" 
-FILE_FORMAT = CSV;
+FILE_FORMAT = "DB_DEMO_DBT"."RAW"."CSV";
 {% endset %}
 
 {% do run_query(sql) %}
